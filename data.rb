fe612a05bdfa3340992c1715f12b390593cb8e48
Nikita Schultz <nikita.schultz1996@gmail.com>
2020-05-09 13:35:01 +0100

    added data file

---
+++ data.rb
@@ -0,0 +1,47 @@
+characters = [
+  {
+    name: "Kaladin Stormblessed",
+    occupation: "soldier",
+    appearance:
+      {
+        hair_colour: "black",
+        eye_colour: "dark_brown"
+      },
+    nationality: "Alethi",
+    abilities: [{surgebinding: "windrunner"}, "shardbearer"],
+    spren: "Syl",
+    relationships:
+      {
+        family:
+          [
+            {
+              name: "Lirin",
+              relation: "father",
+              status: "alive"
+            },
+            {
+              name: "Hesina",
+              relation: "mother",
+              status: "alive"
+            },
+            {
+              name: "Tien",
+              relation: "brother",
+              status: "dead"
+            }
+          ],
+        friends:
+          [
+            {
+              name: "Moash",
+              status: "alive",
+              abilities: [{surgebinding: "windrunner"}]
+            },
+            {
+              name: "Rock",
+              status: "alive",
+              abilities: [{surgebinding: "windrunner"}]
+            }]
+      }
+  }
+]
